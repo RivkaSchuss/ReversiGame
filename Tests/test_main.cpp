209655861c5559befca8c0a221f7e3883d00aa20
Rivka Schuss <rschuss123@gmail.com>
2017-11-26 16:28:43 +0200

    fixing includes

--- Tests/test_main.cpp
+++ Tests/test_main.cpp
@@ -2,9 +2,11 @@
 // Created by Rivka Schuss on 26/11/2017.
 //
 
+//#include "/lib/googletest/googletest/include/gtest/gtest.h"
 #include "../lib/googletest/googletest/include/gtest/gtest.h"
+//#include <gtest/gtest.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
