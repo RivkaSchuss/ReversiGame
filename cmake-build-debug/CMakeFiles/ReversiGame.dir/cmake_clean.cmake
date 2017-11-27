0e78dc7d83be9a5c07bc7f6984e850b98dbd7a90
Rivka Schuss <rschuss123@gmail.com>
2017-11-27 13:20:46 +0200

    changing organization

---
+++ cmake-build-debug/CMakeFiles/ReversiGame.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReversiGame.dir/Board.cpp.o"
+  "CMakeFiles/ReversiGame.dir/Location.cpp.o"
+  "CMakeFiles/ReversiGame.dir/Cell.cpp.o"
+  "CMakeFiles/ReversiGame.dir/AbstractPlayer.cpp.o"
+  "CMakeFiles/ReversiGame.dir/ConsolePlayer.cpp.o"
+  "CMakeFiles/ReversiGame.dir/Game.cpp.o"
+  "CMakeFiles/ReversiGame.dir/AbstractGameLogic.cpp.o"
+  "CMakeFiles/ReversiGame.dir/DefaultGameLogic.cpp.o"
+  "CMakeFiles/ReversiGame.dir/AIPlayer.cpp.o"
+  "CMakeFiles/ReversiGame.dir/main.cpp.o"
+  "ReversiGame.pdb"
+  "ReversiGame"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ReversiGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/rivkaschuss/CLionProjects/ReversiGame/lib/googletest/googlemock
+# Build directory: /Users/rivkaschuss/CLionProjects/ReversiGame/cmake-build-debug/lib/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
