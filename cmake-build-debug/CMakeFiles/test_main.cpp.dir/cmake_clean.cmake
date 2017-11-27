deaaf874762716dbaae25517f90c3b8b7e36f172
Rivka Schuss <rschuss123@gmail.com>
2017-11-27 13:26:55 +0200

    delete files

--- cmake-build-debug/CMakeFiles/test_main.cpp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "test_main.cpp.pdb"
-  "test_main.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_main.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
