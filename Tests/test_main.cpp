bf7f7a6b08a691400847c2a1c5b9b3ca6e7eacf5
Rivka Schuss <rschuss123@gmail.com>
2017-11-27 16:27:59 +0200

    added logic test

--- Tests/test_main.cpp
+++ Tests/test_main.cpp
@@ -4,7 +4,8 @@
 
 #include <gtest/gtest.h>
 #include "../Board.h"
-
+#include "../DefaultGameLogic.h"
+#include "../ConsolePlayer.h"
 
 
 int main(int argc, char** argv) {
@@ -16,4 +17,16 @@ int main(int argc, char** argv) {
 TEST(board__Test, board_Test) {
     Board board(8);
     EXPECT_EQ(8, board.getSize());
+}
+
+TEST(logic_possibleMoves_Test, logic_test) {
+    Player *secondPlayer  =  new ConsolePlayer(white);
+    GameLogic* logic = new DefaultGameLogic(secondPlayer);
+    Board board(8);
+    Location* firstMove = logic->getPossibleMoves(board.getTable(), 8);
+    EXPECT_EQ(firstMove->getRow(), 6);
+    EXPECT_EQ(firstMove->getCol(), 5);
+    delete secondPlayer;
+    delete logic;
+    delete firstMove;
 }
