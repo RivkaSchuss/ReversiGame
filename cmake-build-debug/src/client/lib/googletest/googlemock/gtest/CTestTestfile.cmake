838a9adc42dfa0d69a8dfe9d7b0816ff7feff422
Rivka Schuss <rschuss123@gmail.com>
2018-01-14 19:35:40 +0200

    creating thread pull

--- cmake-build-debug/src/client/lib/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/rivkaschuss/CLionProjects/ReversiGame/src/client/lib/googletest/googletest
-# Build directory: /Users/rivkaschuss/CLionProjects/ReversiGame/cmake-build-debug/src/client/lib/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
